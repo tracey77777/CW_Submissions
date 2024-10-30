28dac4f863b497d02de369b63c854c02c902701a
tracey77777 <traceynguyenly@gmail.com>
2024-10-30 10:29:11 -1000

    Create Logic Coding Bat 1.java

---
+++ Logic Coding Bat 1.java
@@ -0,0 +1,93 @@
+public boolean cigarParty(int cigars, boolean isWeekend) {
+  
+  if (isWeekend)
+  {
+    return (cigars >= 40);
+  }
+  else 
+  {
+    return (cigars <= 60 && cigars >= 40);
+  }
+
+}
+
+
+
+
+public int dateFashion(int you, int date) {
+  if(you <= 2 || date <= 2)
+  {
+    return 0;
+  }
+ else if(you >= 8 || date >= 8)
+  {
+    return 2;
+  }
+  else
+  {
+    return 1;
+  }
+}
+
+
+
+
+
+
+public int caughtSpeeding(int speed, boolean isBirthday) {
+  if (isBirthday)
+  {
+    if (speed <= 65)
+    {
+      return 0;
+    }
+    if (speed <= 85 && speed >= 61)
+    {
+      return 1;
+    }
+    if (speed >= 86)
+    {
+      return 2;
+    }
+
+  }
+  
+   
+  else 
+  {
+    if (speed <= 60)
+    {
+      return 0;
+    }
+    if (speed <= 80 && speed >= 61)
+    {
+      return 1;
+    }
+    if (speed >= 81)
+    {
+      return 2;
+    }
+  }
+    
+}
+
+
+
+
+
+public boolean squirrelPlay(int temp, boolean isSummer) {
+  if (isSummer)
+  {
+    return (temp >= 60 && temp <= 100);
+  }
+  else 
+  {
+    return (temp >= 60 && temp <= 90);
+  }
+  
+}
+
+
+
+
+
