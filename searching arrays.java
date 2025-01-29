586b18fb227af0ff17e79424664b05529bfa8441
tracey77777 <traceynguyenly@gmail.com>
2025-01-29 12:57:08 -1000

    Update searching arrays.java

--- searching arrays.java
+++ searching arrays.java
@@ -1,4 +1,44 @@
+
+public class Searching_Arrays_Intro 
+{
 	
+	
+	// Create a method checkFor4() that is passed an int array and returns true if that array has a 4 and false if that array has no 4's. 1. Create a method checkFor4() that is passed an int array and returns true if that array has a 4 and false if that array has no 4's. 
+	public static boolean checkfor4(int[] arr, int a)
+	{
+		int[] copyArr = arr;
+		boolean isFound = false;
+		for(int i = 0; i < arr.length; i++)
+		{
+			if(a == copyArr[i])
+			{
+				isFound = true;
+			}
+		}
+		return isFound;
+	}
+
+
+	
+	// Create a method countFor4() that is passed an int array and returns how many times 4 shows up in the array. 
+	public static int countElems(int[] arr)
+	{
+	    int[] copyArr = arr; 
+	    int count = 0;
+	    int a = 4;
+	    for(int i = 0; i < arr.length; i++)
+	    {
+	    	if(a == copyArr[i])
+	    	{
+	      	count++;
+	   	}
+	     }
+	     return count;
+	}
+
+	
+	
+	// Create a method checkForFour4() that is passed an int array and return true if the array has exactly four 4's in it. 
 	public static boolean checkForFour4(int[] arr, int a)
 	{
 		int [] copyArr = arr;
@@ -17,3 +57,7 @@
 			}
 			return four4;
 	}
+}
+
+
+
